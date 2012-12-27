78ec8dd10a0afa2627542ff5271b1cc33def305e
Jennifer Pinkham <jpinkham@cpan.org>
2012-12-27 04:32:24 +0000

    created two sep error checking subs. Added post_event() sub and tests.  check for invalid alert_type is not failing when expected. Added TODOs for more tests that need to be added. Almost complete

--- lib/WebService/DataDog/Event.pm
+++ lib/WebService/DataDog/Event.pm
@@ -90,7 +90,7 @@ sub search
 	my $verbose = $self->verbose();
 
 	# Perform various error checks before attempting to search events
-	$self->_error_checks( %args );
+	$self->_search_error_checks( %args );
 	
 	my $url = $WebService::DataDog::API_ENDPOINT . 'events' . '?';
 		
@@ -168,13 +168,144 @@ sub get_event
 }
 
 
-=head1 INTERNAL FUNCTIONS
+=head2 post_event()
 
-=head2 _error_checks()
+Post event to DataDog event stream. This will overlay red areas on all dashboards,
+corresponding to each event.  Example uses: code pushes, server/service restarts, etc.
+
+Per DataDog: "This end point allows you to post events to the stream. You can
+tag them, set priority and event aggregate them with other events."
+
+	my $event = $datadog->build('Event');
+	$event->post_event(
+		title            => $event_title,               
+		text             => $event_text,  # Body/Description of the event.
+		date_happened    => $timestamp,   # Optional, default "now"
+		priority         => $priority,    # Optional. normal|low
+		related_event_id => $event_id,    # Optional, id of aggregate event
+		tags             => $tag_list,    # Optional - tags to apply to event (easy to search by)
+		alert_type       => $alert_type,  # Optional. error|warning|info|success
+		aggregation_key  => $agg_key,     # Optional. Arbitrary string to use for aggregation.
+		source_type_name => $source_type, # Optional. nagios|hudson|jenkins|user|my apps|feed|chef|puppet|git|bitbucket|fabric|capistrano
+	);
+	
+	Examples:
+	+ Submit a user event, with timestamp of `now`.
+	$event->post_event(
+		title            => 'Test event',
+		text             => 'Testing posting to event stream',
+		source_type_name => 'user',
+	);
+	
+Parameters:
+
+=over 4
+
+=item * title
+
+The event title.
+
+=item * text
+
+Optional. Event body/description.
+
+=item * date_happened
+
+Optional. Default value 'now'. POSIX/Unix time.
+
+=item * priority
+
+Optional. Allowed values: normal, low.
+
+=item * related_event_id
+
+Optional. The id of the aggregate event.
+
+=item * tags
+
+Optional. List of tags associated with the event.
+
+=item * alert_type
+
+Optional. "error", "warning", "info" or "success"
+
+=item * aggregation_key
+
+Optional. An arbitrary string to use for aggregation.
+
+=item * source_type_name
+
+Optional. The type of event being posted. Allowed values: nagios, hudson,
+jenkins, user, my apps, feed, chef, puppet, git, bitbucket, fabric, capistrano
+
+=back
 
 =cut
 
-sub _error_checks
+sub post_event
+{
+	my ( $self, %args ) = @_;
+	my $verbose = $self->verbose();
+	
+	# Perform various error checks before attempting to send metrics
+	$self->_event_error_checks( %args );
+	
+	my $data = {
+		title => $args{'title'},
+		text  => $args{'text'},
+	};
+	
+	if ( defined( $args{'date_happened'} ) )
+	{
+		$data->{'date_happened'} = $args{'date_happened'};
+	}
+	
+	if ( defined $args{'priority'} )
+	{
+		$data->{'priority'} = $args{'priority'};
+	}
+	
+	if ( defined( $args{'related_event_id'} ) )
+	{
+		$data->{'related_event_id'} = $args{'related_event_id'};
+	}
+	
+	if ( defined( $args{'tags'} ) )
+	{
+		$data->{'tags'} = $args{'tags'};
+	}
+	
+	if ( defined( $args{'alert_type'} ) )
+	{
+		$data->{'alert_type'} = $args{'alert_type'};
+	}
+	
+	if ( defined( $args{'source_type_name'} ) )
+	{
+		$data->{'source_type_name'} = $args{'source_type_name'};
+	}
+	
+	my $url = $WebService::DataDog::API_ENDPOINT . 'events';
+	
+	my $response = $self->_send_request(
+		method => 'POST',
+		url    => $url,
+		data   => $data,
+	);
+	
+	#TODO check that response is status:ok
+	
+	return $response;
+}
+
+
+=head1 INTERNAL FUNCTIONS
+
+=head2 _search_error_checks()
+
+=cut
+
+sub _search_error_checks
 {
 	my ( $self, %args ) = @_;
 	my $verbose = $self->verbose();
@@ -224,6 +355,72 @@ sub _error_checks
 	
 	return;
 }
+
+
+
+=head2 _event_error_checks()
+
+=cut
+
+sub _event_error_checks
+{
+	my ( $self, %args ) = @_;
+	my $verbose = $self->verbose();
+	
+	# Check for mandatory parameters
+	foreach my $arg ( qw( title text ) )
+	{
+		croak "Argument '$arg' is required for search()"
+			if !defined( $args{$arg} ) || ( $args{$arg} eq '' );
+	}
+	
+	# Check that 'date_happened' is valid
+	if ( defined( $args{'date_happened'} ) )
+	{
+		croak "ERROR - invalid value >" . $args{'date_happened'} . "< for argument 'date_happened'. Must be POSIX/Unixtime"
+			unless ( $args{'date_happened'} =~ /^\d{10,}$/ ); #min 10 digits, allowing for older data back to 1/1/2000
+	}
+	
+	# Check that 'priority' is valid
+	if ( defined $args{'priority'} )
+	{
+		croak "Invalid value >" . $args{'priority'} . "< for argument 'priority'. Allowed values: low, normal."
+			unless ( lc( $args{'priority'} ) eq "low" || lc( $args{'priority'} ) eq "normal" );
+	}
+	
+	# Check that 'related_event_id' is valid
+	if ( defined( $args{'related_event_id'} ) )
+	{
+		croak 'Invalid "related_event_id" >' . $args{'related_event_id'} . '<'
+			unless $args{'related_event_id'} =~ /^\d+$/;
+	}
+	
+	# Check that 'tags' is valid
+	if ( defined( $args{'tags'} ) )
+	{
+		if ( !Data::Validate::Type::is_arrayref( $args{'tags'} ) )
+		{
+			croak "Tag list is invalid. Must be an arrayref.";
+		}
+	}
+	
+	# Check that 'alert_type' is valid
+	if ( defined( $args{'alert_type'} ) )
+	{
+		croak "alert_type >" . $args{'alert_type'} . "< is invalid. Allowed values: error|warning|info|success"
+			unless $args{'alert_type'} =~ /[error|warning|info|success]/;
+	}
+	
+	# Check that 'source_type_name' is valid
+	if ( defined( $args{'source_type_name'} ) )
+	{
+		croak "source_type_name >" . $args{'source_type_name'} . "< is invalid. Allowed values: nagios|hudson|jenkins|user|my apps|feed|chef|puppet|git|bitbucket|fabric|capistrano"
+			unless $args{'source_type_name'} =~ /[nagios|hudson|jenkins|user|my apps|feed|chef|puppet|git|bitbucket|fabric|capistrano]/;
+	}
+	
+	return;
+}
+
 
 =head1 AUTHOR
 
---
+++ t/Event/40-post_event.t
@@ -0,0 +1,175 @@
+#!perl -T
+
+use strict;
+use warnings;
+
+use Data::Dumper;
+
+use Test::Exception;
+use Test::More;
+
+use WebService::DataDog;
+
+
+eval 'use DataDogConfig';
+$@
+	? plan( skip_all => 'Local connection information for DataDog required to run tests.' )
+	: plan( tests => 14 );
+
+my $config = DataDogConfig->new();
+
+# Create an object to communicate with DataDog
+my $datadog = WebService::DataDog->new( %$config );
+ok(
+	defined( $datadog ),
+	'Create a new WebService::DataDog object.',
+);
+
+
+my $event_obj = $datadog->build('Event');
+ok(
+	defined( $event_obj ),
+	'Create a new WebService::DataDog::Event object.',
+);
+my $response;
+
+
+throws_ok(
+	sub
+	{
+		$response = $event_obj->post_event();
+	},
+	qr/Argument.*required/,
+	'Dies on missing arguments.',
+);
+
+throws_ok(
+	sub
+	{
+		$response = $event_obj->post_event( title => "abc" );
+	},
+	qr/Argument.*text.*required/,
+	'Dies on blank/missing "text" argument.',
+);
+
+throws_ok(
+	sub
+	{
+		$response = $event_obj->post_event( 
+			text => "yadda yadda",
+			title => "",
+		);
+	},
+	qr/Argument.*title.*required/,
+	'Dies on blank/missing "title" argument.',
+);
+
+throws_ok(
+	sub
+	{
+		$response = $event_obj->post_event(
+			title => "yadda",
+			text  => "",
+		);
+	},
+	qr/Argument.*text.*required/,
+	'Dies on blank/missing "text" argument.',
+);
+
+throws_ok(
+	sub
+	{
+		$response = $event_obj->post_event(
+			title           => "title goes here",
+			text            => "Text goes here",
+			date_happened   => "abc",
+		);
+	},
+	qr/invalid.*date_happened/,
+	'Dies on invalid "date_happened".',
+);
+
+
+throws_ok(
+	sub
+	{
+		$response = $event_obj->post_event(
+			title    => "title goes here",
+			text     => "Text goes here",
+			priority => "nuclear",
+		);
+	},
+	qr/nvalid.*priority/,
+	'Dies on invalid priority.',
+);
+
+throws_ok(
+	sub
+	{
+		$response = $event_obj->post_event(
+			title            => "title goes here",
+			text             => "Text goes here",
+			related_event_id => "abc",
+		);
+	},
+	qr/nvalid.*related_event_id/,
+	'Dies on invalid related_event_id.',
+);
+
+throws_ok(
+	sub
+	{
+		$response = $event_obj->post_event(
+			title => "title goes here",
+			text  => "Text goes here",
+			tags  => "tags_go_here",
+		);
+	},
+	qr/ag list.*arrayref/,
+	'Dies on invalid tag list.',
+);
+
+throws_ok(
+	sub
+	{
+		$response = $event_obj->post_event(
+			title      => "title goes here",
+			text       => "Text goes here",
+			alert_type => "kabooom",
+		);
+	},
+	qr/nvalid.*alert_type/,
+	'Dies on invalid alert_type.',
+);
+
+my $response;
+lives_ok(
+	sub
+	{
+		$response = $event_obj->post_event(
+			title      => "title goes here",
+			text       => "Text goes here",
+		);
+	},
+	'Post valid event to stream.',
+);
+
+#TODO test with date_happened
+#TODO test with priority
+#TODO test with related_event_id
+#TODO test with tags
+#TODO test with alert_type
+#TODO test with source_type_name
+
+
+
+ok(
+	defined( $response ),
+	'Response was received.'
+);
+
+ok(
+	Data::Validate::Type::is_hashref( $response ),
+	'Response is an hashref.',
+);
+
